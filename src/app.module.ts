f09043b870c68b47e64aae63026fcc5530df7fa5
zakarya <zourgui.zakarya@gmail.com>
2026-04-14 16:38:10 +0100

    update the app.module the database are now in env not public

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({
       type: 'mariadb',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'typeorm2',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       entities: ['dist/**/*.entity.js'],
       synchronize: false,// this will create the tables in the database based on the entities we have in our project, but it will not update the tables if we change the entities, so we need to run the migration to update the tables in the database
       }), 
